958ec1ca112edcc2a18fc25d9b3f5bff24d3cdb7
Jorge <jorge.filipepinto@hotmail.com>
2024-08-18 17:38:34 +0100

    reorganizacao do codigo verificacoes e controlos ao criar sms

--- app/src/main/java/com/example/digi/MainActivity.java
+++ app/src/main/java/com/example/digi/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
@@ -20,16 +19,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    String txtSms, noTeam, noSiteCode, smsCreated, noSMS, okSms;
+    String txtSms, noTeam, noSiteCode, smsCreated, noSMS, okSms,radioTask, radioStatus, siteCode, teamCode;
 
     Button clTeamCode, clSiteCode, clTroubleFounded, clTroubleFix, clTroubleNeedFix, generateSms, smsCopy;
     ImageButton sendSMSWhatsapp;
     RadioGroup tasks, status;
-    RadioButton siteDown, integration, warehouse, noGw, travelling, start, blocked, onGoing, finish;
+    RadioButton siteDown, integration, warehouse, noGw;
+    RadioButton travelling, start, blocked, onGoing, finish;
     LinearLayout ongoingComment, layoutFinish;
     EditText teamCodeBox, siteCodeBox, ongoingTextComment,troubleFoundedBox, troubleFixBox, troubleNeedToFixBox, smsTextBox;
 
-    boolean isSmsCreated = false, warehouseSelected, onGoingSelected;
+    boolean isSmsCreated = false, smsValid = false, warehouseIsSelected, onGoingIsSelected, finishIsSelected;
     int shortDuration, longDuration;
 
 
@@ -70,17 +70,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         sendSMSWhatsapp = findViewById(R.id.btn_whatsapp_send);
         smsCopy = findViewById(R.id.btn_sms_copy);
 
-        noTeam = getString(R.string.no_team_code_found);
-        noSiteCode = getString(R.string.no_site_code_found);
-        smsCreated = getString(R.string.sms_created);
-        noSMS = getString(R.string.no_sms_found);
-        okSms = getString(R.string.sms_founded);
-
         travelling.setOnClickListener(this);
         start.setOnClickListener(this);
         blocked.setOnClickListener(this);
         onGoing.setOnClickListener(this);
         finish.setOnClickListener(this);
+        siteDown.setOnClickListener(this);
+        integration.setOnClickListener(this);
+        warehouse.setOnClickListener(this);
+        noGw.setOnClickListener(this);
+
 
         StartApp();
 
@@ -96,117 +95,135 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
         generateSms.setOnClickListener(view -> {
-            smsCopy.setVisibility(View.VISIBLE);
-            String teamCode = teamCodeBox.getText().toString().toUpperCase() + "_";
-            String siteCode = siteCodeBox.getText().toString().toUpperCase() + "_";
-            String radioTask = decodeTasks(tasks.getCheckedRadioButtonId()) + "_";
-            String radioStatus = decodeStatus((status.getCheckedRadioButtonId()));
-            String strTroubleFind = troubleFoundedBox.getText().toString();
-            String strTroubleFix = troubleFixBox.getText().toString();
-            String strTroubleNeedFix = troubleNeedToFixBox.getText().toString();
-
-            if(warehouseSelected && checkTeam()){
-                txtSms = teamCode + radioTask + "travelling";
-                ClearBoxes();
-                smsTextBox.setText(txtSms);
-            }else{
-                if(onGoingSelected && checkTeam() && checkSiteCode()){
-                    txtSms = teamCode + siteCode + radioTask + radioStatus;
-                    ClearBoxes();
-                    smsTextBox.setText(txtSms);
-                }else{
-                    if(checkTeam() && checkSiteCode()) {
-                        txtSms = teamCode + siteCode + radioTask + radioStatus +
-                                getString(R.string.trouble_found) + strTroubleFind + getString(R.string.trouble_fix) +
-                                strTroubleFix + getString(R.string.trouble_to_fix) + strTroubleNeedFix;
-                        ClearBoxes();
-                        smsTextBox.setText(txtSms);
-                    }
-                }
+            sms();
+            if(smsValid)
+            {
+                smsCopy.setVisibility(View.VISIBLE);
+                smsTextBox.setVisibility(View.VISIBLE);
+            }
+            else
+            {
+                sendToast("Menssagem inválida", longDuration);
+                smsCopy.setVisibility(View.INVISIBLE);
+                smsTextBox.setVisibility(View.INVISIBLE);
             }
         });
 
         sendSMSWhatsapp.setOnClickListener(view -> {
-            if(tasks.getCheckedRadioButtonId() == R.id.radio_btn_warehouse){
+            if(tasks.getCheckedRadioButtonId() == R.id.radio_btn_warehouse)
+            {
                 WhatsAppSend(txtSms);
-            }else {
-                if (!teamCodeBox.getText().toString().isEmpty() || !siteCodeBox.getText().toString().isEmpty() || !isSmsCreated) {
+            }
+            else
+            {
+                if (!teamCodeBox.getText().toString().isEmpty() || !siteCodeBox.getText().toString().isEmpty() || !isSmsCreated)
+                {
                     WhatsAppSend(txtSms);
-                } else {
+                }
+                else
+                {
                     sendToast(noSMS, longDuration);
                 }
             }
         });
 
         smsCopy.setOnClickListener(view -> {
-            String outputText = smsTextBox.getText().toString();
-            if (outputText.isEmpty()) {
-                sendToast(noSMS, shortDuration);
-
-            }else{
-                ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                ClipData clipData = ClipData.newPlainText("SMS", outputText);
-                clipboardManager.setPrimaryClip(clipData);
-                sendToast(okSms, shortDuration);
-            }
+            smsCopied();
         });
     }
 
-    String decodeTasks(int task){
-        String taskDecoded = "";
-        if(task == R.id.radio_btn_site_down){
-            taskDecoded = "SITE_DOWN";
-            warehouseSelected = false;
-        }else if(task == R.id.radio_btn_integration){
-            taskDecoded = "INTEGRATION";
-            warehouseSelected = false;
-        }else if(task == R.id.radio_btn_warehouse){
-            taskDecoded = "WAREHOUSE";
-            warehouseSelected = true;
-        }else if(task == R.id.radio_btn_non_gw_gw){
-            taskDecoded = "NON_GW->GW";
-            warehouseSelected = false;
-        }
-        return taskDecoded;
-    }
-
-    String decodeStatus(int status){
-        String statusDecoded = "";
-        if(status == R.id.radio_btn_travelling){
-            statusDecoded = "TRAVELLING";
-            onGoingSelected = false;
-        }else if(status == R.id.radio_btn_start){
-            statusDecoded = "START";
-            onGoingSelected = false;
-        }else if(status == R.id.radio_btn_blocked){
-            statusDecoded = "BLOCKED";
-            onGoingSelected = false;
-        }else if(status == R.id.radio_btn_ongoing){
-            statusDecoded = "ONGOING";
-            onGoingSelected = true;
-        }else if(status == R.id.radio_btn_finish){
-            statusDecoded = "FINISH";
-            onGoingSelected = false;
-        }
-        return statusDecoded;
-    }
-
-
-    @Override
-    public void onClick(View view){
-        if(view.getId() == finish.getId()) {
-            layoutFinish.setVisibility(View.VISIBLE);
-        } else {layoutFinish.setVisibility(View.GONE);}
-
-        if(view.getId() == onGoing.getId()){
-            ongoingComment.setVisibility(View.VISIBLE);
-        }else{ongoingComment.setVisibility(View.GONE);}
-    }
-
-    void StartApp(){
+    void StartApp()
+    {
+        radioTask = "_SITE_DOWN";
+        radioStatus = "_TRAVELLING";
         layoutFinish.setVisibility(View.GONE);
         ongoingComment.setVisibility(View.GONE);
-        smsTextBox.setText(getString(R.string.sms_box_text_default));
+        errorMessages();
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        decodeTasks(view.getId());
+        decodeStatus(view.getId());
+        if(view.getId() == finish.getId())
+        {
+            layoutFinish.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            layoutFinish.setVisibility(View.GONE);
+        }
+
+        if(view.getId() == onGoing.getId())
+        {
+            ongoingComment.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            ongoingComment.setVisibility(View.GONE);
+        }
+    }
+
+
+
+    void decodeTasks(int task)
+    {
+        if(task == R.id.radio_btn_site_down)
+        {
+            radioTask = "_SITE_DOWN";
+            warehouseIsSelected = false;
+        }
+        else if(task == R.id.radio_btn_integration)
+        {
+            radioTask = "_INTEGRATION";
+            warehouseIsSelected = false;
+        }
+        else if(task == R.id.radio_btn_warehouse)
+        {
+            radioTask = "_WAREHOUSE";
+            warehouseIsSelected = true;
+        }
+        else if(task == R.id.radio_btn_non_gw_gw)
+        {
+            radioTask = "_NON_GW->GW";
+            warehouseIsSelected = false;
+        }
+    }
+
+    void decodeStatus(int status)
+    {
+        if(status == R.id.radio_btn_travelling)
+        {
+            radioStatus = "_TRAVELLING";
+            onGoingIsSelected = false;
+            finishIsSelected = false;
+        }
+        else if(status == R.id.radio_btn_start)
+        {
+            radioStatus = "_START";
+            onGoingIsSelected = false;
+            finishIsSelected = false;
+        }
+        else if(status == R.id.radio_btn_blocked)
+        {
+            radioStatus = "_BLOCKED";
+            onGoingIsSelected = false;
+            finishIsSelected = false;
+        }
+        else if(status == R.id.radio_btn_ongoing)
+        {
+            radioStatus = "_ONGOING";
+            onGoingIsSelected = true;
+            finishIsSelected = false;
+        }
+        else if(status == R.id.radio_btn_finish)
+        {
+            radioStatus = "_FINISH";
+            onGoingIsSelected = false;
+            finishIsSelected = true;
+        }
+
     }
 
     void ClearBoxes(){
@@ -215,11 +232,145 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         troubleNeedToFixBox.setText("");
     }
 
-    void sendToast(String text, int duration){
+    void sendToast(String text, int duration)
+    {
         Toast.makeText(this, text, duration).show();
     }
 
-    void WhatsAppSend(String sms){
+    boolean setTeam()
+    {
+        boolean teamCheck = false;
+        if(!teamCodeBox.getText().toString().isEmpty())
+        {
+            teamCheck = true;
+            teamCode = teamCodeBox.getText().toString().toUpperCase() + "_";
+        }
+        return teamCheck;
+    }
+
+    boolean setSiteCode()
+    {
+        boolean siteCheck = false;
+        if(!siteCodeBox.getText().toString().isEmpty())
+        {
+            siteCheck = true;
+            siteCode = siteCodeBox.getText().toString().toUpperCase();
+        }
+        return siteCheck;
+    }
+
+    void errorMessages()
+    {
+        noTeam = getString(R.string.no_team_code_found);
+        noSiteCode = getString(R.string.no_site_code_found);
+        smsCreated = getString(R.string.sms_created);
+        noSMS = getString(R.string.no_sms_found);
+        okSms = getString(R.string.sms_founded);
+    }
+
+
+    void sms()
+    {
+        setTeam();
+        setSiteCode();
+        String strOnGoing = "";
+        String strTroubleFind = "";
+        String strTroubleFix = "";
+        String strTroubleNeedFix = "";
+        String strFinish = "";
+
+        if(!ongoingTextComment.getText().toString().isEmpty())
+        {
+            strOnGoing = getString(R.string.ongoing_comment) + ongoingTextComment.getText().toString();
+        }
+
+        if(!troubleFoundedBox.getText().toString().isEmpty())
+        {
+            strTroubleFind = getString(R.string.trouble_found) + troubleFoundedBox.getText().toString();
+            strFinish += strTroubleFind;
+        }
+
+        if(!troubleFixBox.getText().toString().isEmpty())
+        {
+            strTroubleFix = getString(R.string.trouble_fix) + troubleFixBox.getText().toString();
+            strFinish += strTroubleFix;
+        }
+
+        if(!troubleNeedToFixBox.getText().toString().isEmpty())
+        {
+            strTroubleNeedFix = getString(R.string.trouble_to_fix) + troubleNeedToFixBox.getText().toString();
+            strFinish += strTroubleNeedFix;
+        }
+
+        if(warehouseIsSelected)
+        {
+            if(setTeam()) {
+                txtSms = teamCode + radioTask + "_TRAVELLING";
+                smsTextBox.setText(txtSms);
+                smsValid = true;
+            }
+            else
+            {
+                smsValid = false;
+                sendToast(noTeam, longDuration);
+            }
+        }
+        else if(onGoingIsSelected)
+        {
+            if(!warehouseIsSelected)
+            {
+                txtSms = teamCode + siteCode + radioTask + radioStatus + strOnGoing;
+                smsTextBox.setText(txtSms);
+                smsValid = true;
+            }
+            else {
+                sendToast("Selecione outra tarefa", longDuration);
+                smsValid = false;
+            }
+        }
+        else if(finishIsSelected)
+        {
+            if (!warehouseIsSelected)
+            {
+                txtSms = teamCode + siteCode + radioTask + radioStatus + strFinish;
+                ClearBoxes();
+                smsTextBox.setText(txtSms);
+                smsValid = true;
+            }
+            else
+            {
+                sendToast("Selecione outra tarefa", longDuration);
+                smsValid = false;
+            }
+        }
+        else
+            {
+                if(teamCodeBox.getText().toString().isEmpty())
+                {
+                    sendToast(noTeam, longDuration);
+                    smsValid = false;
+                }
+                else
+                {
+                    if(siteCodeBox.getText().toString().isEmpty())
+                    {
+                       sendToast(noSiteCode,longDuration);
+                        smsValid = false;
+                    }
+                    else
+                    {
+                        txtSms = teamCode + siteCode + radioTask + radioStatus;
+                        smsTextBox.setText(txtSms);
+                        smsValid = true;
+                    }
+                }
+
+            }
+    }
+
+
+    void WhatsAppSend(String sms)
+    {
         Intent sendWhatsapp = new Intent();
         sendWhatsapp.putExtra(Intent.EXTRA_TEXT, sms);
         sendWhatsapp.setPackage("com.whatsapp");
@@ -227,20 +378,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startActivity(sendWhatsapp);
     }
 
-    boolean checkTeam(){
-        boolean teamCheck = false;
-        if(teamCodeBox.getText().toString().isEmpty()){
-            sendToast(noTeam, longDuration);
-        }else{teamCheck = true;}
-        return teamCheck;
-    }
+    void smsCopied()
+    {
+        String outputText = smsTextBox.getText().toString();
+        if (outputText.isEmpty()) {
+            sendToast(noSMS, shortDuration);
 
-    boolean checkSiteCode() {
-        boolean siteCheck = false;
-        if(siteCodeBox.getText().toString().isEmpty()){
-            sendToast(noSiteCode, longDuration);
-        }else{siteCheck = true;}
-        return siteCheck;
+        }else{
+            ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+            ClipData clipData = ClipData.newPlainText("SMS", outputText);
+            clipboardManager.setPrimaryClip(clipData);
+            sendToast(okSms, shortDuration);
+        }
     }
-
 }
