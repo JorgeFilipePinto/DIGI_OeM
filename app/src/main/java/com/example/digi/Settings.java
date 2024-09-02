e9904a04aa0f8ff38456b0c46258a44feeeb23d5
JorgeFilipePinto <jorge.filipepinto@hotmail.com>
2024-09-02 21:44:34 +0100

    settings.java variables

--- app/src/main/java/com/example/digi/Settings.java
+++ app/src/main/java/com/example/digi/Settings.java
@@ -3,6 +3,8 @@ package com.example.digi;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
@@ -17,11 +19,18 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         ImageButton home;
+        EditText settingOne, settingTwo, settingThree, settingFour;
+        Button saveButton;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
         home = findViewById(R.id.home_button);
+        settingOne = findViewById(R.id.setting1);
+        settingTwo = findViewById(R.id.setting2);
+        settingThree = findViewById(R.id.setting3);
+        settingFour = findViewById(R.id.setting4);
+        saveButton = findViewById(R.id.save_settings);
 
 
         home.setOnClickListener(new View.OnClickListener() {
@@ -31,6 +40,5 @@ public class Settings extends AppCompatActivity {
                 startActivity(goHome);
             }
         });
-
     }
 }
